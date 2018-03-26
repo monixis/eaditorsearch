4d1e57e9361fd96d0b108261d37af842f42b57e1
Zack <spalding@ip-172-31-30-49.ec2.internal>
2018-03-26 16:09:18 -0400

    made a minor change in regards to the http reference

--- application/views/search.php
+++ application/views/search.php
@@ -21,7 +21,7 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script type="text/javascript" src="https://beta.empireadc.org/js/jquery-ui.js"></script>
+		<script type="text/javascript" src="//beta.empireadc.org/js/jquery-ui.js"></script>
 		<link rel="stylesheet" href="//beta.empireadc.org/font-awesome/css/font-awesome.min.css">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 		<script src="./js/nprogress.js"></script>
